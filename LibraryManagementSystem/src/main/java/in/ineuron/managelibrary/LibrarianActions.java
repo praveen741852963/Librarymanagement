a6d462084d192e600273c8686d23f1003c9e6667
praveen <Praveen chaitra@LAPTOP-5VU5M6TV>
2023-05-11 11:53:39 +0530

    First Commit

---
+++ LibraryManagementSystem/src/main/java/in/ineuron/managelibrary/LibrarianActions.java
@@ -0,0 +1,22 @@
+package in.ineuron.managelibrary;
+
+import in.ineuron.dto.*;
+
+public interface LibrarianActions {
+	String addbook(String name,String Author,String subject,int quantity);
+	String deletebook(int bid);
+	String updatebook(Book Book);
+	Book searchbook(int bid);
+	String addstudent(String name,String branch,long conno);
+	String  deletestudent(int sid);
+	String updatestudent(Student student);
+	Student searchstudent(int sid);
+	String issuebook(int sid,int bid);
+	int returnbook(int sid,int bid);
+	String Studentreg(Student student);
+	String Studentlog(String email,String password);
+	String Librarianreg(Librarian L);
+	String Librarianlog(String email,String password);
+	
+	
+}
---
+++ LibraryManagementSystem/src/main/java/in/ineuron/util/jdbcUtil.java
@@ -0,0 +1,26 @@
+package in.ineuron.util;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.sql.*;
+import java.util.Properties;
+
+public class jdbcUtil {
+
+
+	public static Connection jdbcconnection() throws ClassNotFoundException, SQLException, IOException {
+		Class.forName("com.mysql.cj.jdbc.Driver");
+		System.out.println("Driver is loading");
+	//make the connection to the sql database(jdbc)
+	FileInputStream f=new FileInputStream("C:\\Users\\Praveen chaitra\\eclipse-workspace\\LibraryManagementSystem\\src\\main\\java\\jdbc.properties");
+	Properties p=new Properties();
+	p.load(f);
+	
+			
+		
+		  Connection connection=DriverManager.getConnection(p.getProperty("url"),p.getProperty("username"),p.getProperty("password")); 
+		  return connection;
+	
+	}
+
+}
